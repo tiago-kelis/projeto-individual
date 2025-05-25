b60f596ea609050f058d2862afe8a46914861b55
tiago-kelis <tiagokelis2017@gmail.com>
2025-05-25 18:19:59 -0300

    Resolvendo conflito com Area.tsx

--- frontend/src/components/landing/comuns/Area.tsx
+++ frontend/src/components/landing/comuns/Area.tsx
@@ -1,21 +1,20 @@
 import Conteudo from "../content/Conteudo";
 import MenuLateral from "../dashboard/MenuLateral";
+import Menu from "../header/Menu";
 
 
 
 export default function Area() {
     return (
         <div className="flex flex-col h-screen">
-            <Menu>
-
-            </Menu>
+            <Menu />            
                 <div className="flex">
                     <MenuLateral>
                 
                      </MenuLateral>
                     <Conteudo className="">
                         <div className="text-3xl ml-36 mt-24 ">
-                            Acompanhe o Andamento deste Projeto...
+                            Acompanhe o Andamento do Projeto...
                         </div>
                     </Conteudo>
                 </div>
--- frontend/src/components/landing/dashboard/MenuLateral.tsx
+++ frontend/src/components/landing/dashboard/MenuLateral.tsx
@@ -14,7 +14,7 @@ export default function MenuLateral(props: MenuLateralProps) {
     return (
         <div className={`flex flex-col flex-1 h-screen bg-zinc-900 max-w-52  ${props.className ?? ''}`}>
 
-             <MenuItemLateral icon={<IconVideo/>} label="Videos"/>
+            <MenuItemLateral icon={<IconVideo/>} label="Videos"/>
             <MenuItemLateral icon={<IconUser/>} label="User"/> 
             <MenuItemLateral icon={<IconServer/>} label="Services"/> 
             <MenuItemLateral icon={<IconSettings/>} label="Settings"/> 
